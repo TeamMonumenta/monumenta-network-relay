9fcbc07058d97b3152a0eabcfbafbdbe41e2f251
Byron Marohn <Combustible@live.com>
2022-12-24 19:35:33 +0000

    Update CommandAPI to 8.7.0
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ plugins {
 
 repositories {
     mavenLocal()
+    mavenCentral()
+
     maven {
         url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     }
@@ -55,8 +57,9 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.16.4-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains:annotations:16.0.2")
     implementation("com.rabbitmq:amqp-client:5.8.0")
-    compileOnly("dev.jorel.CommandAPI:commandapi-core:6.0.0")
+    compileOnly("dev.jorel.CommandAPI:commandapi-core:8.7.0")
     compileOnly("net.md-5:bungeecord-api:1.15-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.10.4")
     errorprone("com.google.errorprone:error_prone_core:2.10.0")
