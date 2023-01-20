7eb3adec3b3a8f025d63e50e0c79232918a316aa
Byron Marohn <Combustible@live.com>
2023-01-20 02:42:26 +0000

    Revert "Shade newer CommandAPI temporarily"
    This reverts commit d9e2b0e7bf37550f183172bf2d7384d23a40d5af.
    
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.16.4-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:16.0.2")
     implementation("com.rabbitmq:amqp-client:5.8.0")
-    implementation("dev.jorel.CommandAPI:commandapi-shade:8.7.0")
+    compileOnly("dev.jorel.CommandAPI:commandapi-core:8.7.0")
     compileOnly("net.md-5:bungeecord-api:1.15-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.10.4")
     errorprone("com.google.errorprone:error_prone_core:2.10.0")
@@ -85,6 +85,7 @@ bukkit {
     apiVersion = "1.13"
     name = "MonumentaNetworkRelay"
     authors = listOf("Combustible")
+    depend = listOf("CommandAPI")
     softDepend = listOf("PlaceholderAPI")
 }
 
@@ -93,13 +94,6 @@ bungee {
     name = "MonumentaNetworkRelay"
     main = "com.playmonumenta.networkrelay.NetworkRelayBungee"
     author = "Combustible"
-}
-
-// Relocation / shading
-tasks {
-    shadowJar {
-       relocate("dev.jorel.commandapi", "com.playmonumenta.networkrelay.internal.dev.jorel.commandapi")
-    }
 }
 
 publishing {
