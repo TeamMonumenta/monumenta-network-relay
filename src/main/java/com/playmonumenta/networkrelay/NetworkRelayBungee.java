2c6903e5788dd98dd83214a984b24d91b437b773
Byron Marohn <Combustible@live.com>
2023-06-06 03:56:41 +0000

    Revert "Add threadsafe bungee config provider for use with auto registration"
    This reverts commit 56d483433917de2ec9752473a30e39f3da583520.

--- src/main/java/com/playmonumenta/networkrelay/NetworkRelayBungee.java
+++ src/main/java/com/playmonumenta/networkrelay/NetworkRelayBungee.java
@@ -13,130 +13,99 @@ import net.md_5.bungee.config.YamlConfiguration;
 import org.jetbrains.annotations.Nullable;
 
 public class NetworkRelayBungee extends Plugin {
-	class Config {
-		boolean runReceivedCommands;
-		boolean autoRegisterServersToBungee;
-		boolean autoUnregisterInactiveServersFromBungee;
-		String shardName;
-		String rabbitURI;
-		int heartbeatInterval;
-		int destinationTimeout;
-		long defaultTTL;
-
-		private Config() {
-			File configFile = new File(getDataFolder(), "config.yml");
-
-			/* Create the config file & directories if it does not exist */
-			if (!configFile.exists()) {
-				try {
-					// Create parent directories if they do not exist
-					configFile.getParentFile().mkdirs();
-
-					// Copy the default config file
-					Files.copy(getClass().getResourceAsStream("/default_config_bungee.yml"), configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-				} catch (IOException ex) {
-					getLogger().log(Level.SEVERE, "Failed to create configuration file");
-				}
-			}
-
-			/* Load the config file & parse it */
-			Configuration config;
-			try {
-				config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
-			} catch (IOException ex) {
-				getLogger().warning("Failed to load config file, using defaults: " + ex.getMessage());
-				config = new Configuration();
-			}
-
-			String logLevel = config.getString("log-level", "INFO");
-			runReceivedCommands = config.getBoolean("run-received-commands", true);
-			/* Shard name defaults to environment variable NETWORK_RELAY_NAME if present */
-			shardName = System.getenv("NETWORK_RELAY_NAME");
-			if (shardName == null || shardName.isEmpty()) {
-				shardName = "default-shard";
-			}
-			shardName = config.getString("shard-name", shardName); // Config file overrides env var
-			autoRegisterServersToBungee = config.getBoolean("auto-register-servers-to-bungee", false);
-			autoUnregisterInactiveServersFromBungee = config.getBoolean("auto-unregister-inactive-servers-from-bungee", false);
-			rabbitURI = config.getString("rabbitmq-uri", "amqp://guest:guest@127.0.0.1:5672");
-			heartbeatInterval = config.getInt("heartbeat-interval", 1);
-			destinationTimeout = config.getInt("destination-timeout", 5);
-			defaultTTL = config.getLong("default-time-to-live", 604800);
-
-			/* Echo config */
-			try {
-				getLogger().setLevel(Level.parse(logLevel));
-				getLogger().info("log-level=" + logLevel);
-			} catch (Exception ex) {
-				getLogger().warning("log-level=" + logLevel + " is invalid - defaulting to INFO");
-			}
-			getLogger().info("run-received-commands=" + runReceivedCommands);
-			if (rabbitURI.equals("amqp://guest:guest@127.0.0.1:5672")) {
-				getLogger().info("rabbitmq-uri=<default>");
-			} else {
-				getLogger().info("rabbitmq-uri=<set>");
-			}
-			if (shardName.equals("default-shard")) {
-				getLogger().warning("shard-name is default value 'default-shard' which should be changed!");
-			} else {
-				getLogger().info("shard-name=" + shardName);
-			}
-			getLogger().info("auto-register-servers-to-bungee=" + autoRegisterServersToBungee);
-			if (autoUnregisterInactiveServersFromBungee && !autoRegisterServersToBungee) {
-				getLogger().warning("Config mismatch - auto-unregister-inactive-servers-from-bungee auto-register-servers-to-bungee=false");
-				getLogger().warning("Setting auto-unregister-inactive-servers-from-bungee");
-				autoUnregisterInactiveServersFromBungee = false;
-			}
-			getLogger().info("auto-unregister-inactive-servers-from-bungee=" + autoUnregisterInactiveServersFromBungee);
-
-			if (heartbeatInterval <= 0) {
-				getLogger().warning("heartbeat-interval is <= 0 which is invalid! Using default of 1.");
-				heartbeatInterval = 1;
-			} else {
-				getLogger().info("heartbeat-interval=" + heartbeatInterval);
-			}
-			if (destinationTimeout <= 1) {
-				getLogger().warning("destination-timeout is <= 1 which is invalid! Using default of 5.");
-				destinationTimeout = 5;
-			} else {
-				getLogger().info("destination-timeout=" + destinationTimeout);
-			}
-			if (defaultTTL < 0) {
-				getLogger().warning("default-time-to-live is < 0 which is invalid! Using default of 604800.");
-				defaultTTL = 604800;
-			} else {
-				getLogger().info("default-time-to-live=" + defaultTTL);
-			}
-		}
-	}
-
 	private @Nullable RabbitMQManager mRabbitMQManager = null;
 	private @Nullable CustomLogger mLogger = null;
-	private @Nullable Config mConfig = null;
-
-	@Override
-	public void onLoad() {
-		mConfig = new Config();
-
-		if (mConfig.autoRegisterServersToBungee) {
-			/* Replace the default configuration adapter with a threadsafe variant with similar behavior */
-
-			getLogger().info("Replacing bungee configuration loader with custom thread-safe version");
-			getLogger().info("This may break other plugins that manipulate the bungee config");
-			getProxy().setConfigurationAdapter(new BungeeThreadsafeYamlConfig());
-		}
-	}
 
 	@Override
 	public void onEnable() {
-		if (mConfig == null) {
-			mConfig = new Config();
+		File configFile = new File(getDataFolder(), "config.yml");
+
+		/* Create the config file & directories if it does not exist */
+		if (!configFile.exists()) {
+			try {
+				// Create parent directories if they do not exist
+				configFile.getParentFile().mkdirs();
+
+				// Copy the default config file
+				Files.copy(getClass().getResourceAsStream("/default_config_bungee.yml"), configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException ex) {
+				getLogger().log(Level.SEVERE, "Failed to create configuration file");
+			}
 		}
 
-		getProxy().getPluginManager().registerListener(this, new BungeeNetworkMessageListener(getLogger(), mConfig.runReceivedCommands, mConfig.autoRegisterServersToBungee, mConfig.autoUnregisterInactiveServersFromBungee));
+		/* Load the config file & parse it */
+		Configuration config;
+		try {
+			config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+		} catch (IOException ex) {
+			getLogger().warning("Failed to load config file, using defaults: " + ex.getMessage());
+			config = new Configuration();
+		}
+
+		String logLevel = config.getString("log-level", "INFO");
+		boolean runReceivedCommands = config.getBoolean("run-received-commands", true);
+		/* Shard name defaults to environment variable NETWORK_RELAY_NAME if present */
+		String shardName = System.getenv("NETWORK_RELAY_NAME");
+		if (shardName == null || shardName.isEmpty()) {
+			shardName = "default-shard";
+		}
+		shardName = config.getString("shard-name", shardName); // Config file overrides env var
+		boolean autoRegisterServersToBungee = config.getBoolean("auto-register-servers-to-bungee", false);
+		boolean autoUnregisterInactiveServersFromBungee = config.getBoolean("auto-unregister-inactive-servers-from-bungee", false);
+		String rabbitURI = config.getString("rabbitmq-uri", "amqp://guest:guest@127.0.0.1:5672");
+		int heartbeatInterval = config.getInt("heartbeat-interval", 1);
+		int destinationTimeout = config.getInt("destination-timeout", 5);
+		long defaultTTL = config.getLong("default-time-to-live", 604800);
+
+		/* Echo config */
+		try {
+			getLogger().setLevel(Level.parse(logLevel));
+			getLogger().info("log-level=" + logLevel);
+		} catch (Exception ex) {
+			getLogger().warning("log-level=" + logLevel + " is invalid - defaulting to INFO");
+		}
+		getLogger().info("run-received-commands=" + runReceivedCommands);
+		if (rabbitURI.equals("amqp://guest:guest@127.0.0.1:5672")) {
+			getLogger().info("rabbitmq-uri=<default>");
+		} else {
+			getLogger().info("rabbitmq-uri=<set>");
+		}
+		if (shardName.equals("default-shard")) {
+			getLogger().warning("shard-name is default value 'default-shard' which should be changed!");
+		} else {
+			getLogger().info("shard-name=" + shardName);
+		}
+		getLogger().info("auto-register-servers-to-bungee=" + autoRegisterServersToBungee);
+		if (autoUnregisterInactiveServersFromBungee && !autoRegisterServersToBungee) {
+			getLogger().warning("Config mismatch - auto-unregister-inactive-servers-from-bungee auto-register-servers-to-bungee=false");
+			getLogger().warning("Setting auto-unregister-inactive-servers-from-bungee");
+			autoUnregisterInactiveServersFromBungee = false;
+		}
+		getLogger().info("auto-unregister-inactive-servers-from-bungee=" + autoUnregisterInactiveServersFromBungee);
+
+		if (heartbeatInterval <= 0) {
+			getLogger().warning("heartbeat-interval is <= 0 which is invalid! Using default of 1.");
+			heartbeatInterval = 1;
+		} else {
+			getLogger().info("heartbeat-interval=" + heartbeatInterval);
+		}
+		if (destinationTimeout <= 1) {
+			getLogger().warning("destination-timeout is <= 1 which is invalid! Using default of 5.");
+			destinationTimeout = 5;
+		} else {
+			getLogger().info("destination-timeout=" + destinationTimeout);
+		}
+		if (defaultTTL < 0) {
+			getLogger().warning("default-time-to-live is < 0 which is invalid! Using default of 604800.");
+			defaultTTL = 604800;
+		} else {
+			getLogger().info("default-time-to-live=" + defaultTTL);
+		}
+
+		getProxy().getPluginManager().registerListener(this, new BungeeNetworkMessageListener(getLogger(), runReceivedCommands, autoRegisterServersToBungee, autoUnregisterInactiveServersFromBungee));
 
 		try {
-			mRabbitMQManager = new RabbitMQManager(new RabbitMQManagerAbstractionBungee(this), getLogger(), mConfig.shardName, mConfig.rabbitURI, mConfig.heartbeatInterval, mConfig.destinationTimeout, mConfig.defaultTTL);
+			mRabbitMQManager = new RabbitMQManager(new RabbitMQManagerAbstractionBungee(this), getLogger(), shardName, rabbitURI, heartbeatInterval, destinationTimeout, defaultTTL);
 		} catch (Exception e) {
 			getLogger().severe("RabbitMQ manager failed to initialize. This plugin will not function");
 			e.printStackTrace();
