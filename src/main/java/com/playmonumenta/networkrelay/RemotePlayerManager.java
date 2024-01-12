30c1fe00942254b26863c73b7b18c2b6b957560e
Ascynx <miaoum2128@gmail.com>
2024-01-12 18:27:08 +0100

    Moved stuff around to make it follow how it was formatted in the network-chat counterpart.

--- src/main/java/com/playmonumenta/networkrelay/RemotePlayerManager.java
+++ src/main/java/com/playmonumenta/networkrelay/RemotePlayerManager.java
@@ -22,13 +22,88 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class RemotePlayerManager implements Listener {
+	public static class RemotePlayer {
+		public final UUID mUuid;
+		public final String mName;
+		public final String mShard;
+		public final boolean mIsHidden;
+		public final boolean mIsOnline;
+		private final ConcurrentMap<String, JsonObject> mPluginData;
+
+		public RemotePlayer(Player player, boolean isOnline) {
+			mUuid = player.getUniqueId();
+			mName = player.getName();
+			RemotePlayerManager manager = RemotePlayerManager.getInstance();
+			mShard = (manager != null && manager.getShardName() != null) ? manager.getShardName() : NetworkRelayAPI.getShardName();
+			mIsHidden = (manager != null && !manager.isLocalPlayerVisible(player));
+			mIsOnline = isOnline;
+			mPluginData = new ConcurrentHashMap<>();
+		}
+
+		public RemotePlayer(JsonObject remoteData) {
+			mUuid = UUID.fromString(remoteData.get("playerUuid").getAsString());
+			mName = remoteData.get("playerName").getAsString();
+			mIsHidden = remoteData.get("isHidden").getAsBoolean();
+			mIsOnline = remoteData.get("isOnline").getAsBoolean();
+			mShard = remoteData.get("shard").getAsString();
+			mPluginData = serializePluginData(remoteData);
+		}
+
+		@Nullable
+		public JsonObject getPluginData(String pluginId) {
+			return mPluginData.get(pluginId);
+		}
+
+		private ConcurrentMap<String, JsonObject> serializePluginData(JsonObject remoteData) {
+			ConcurrentMap<String, JsonObject> pluginDataMap = new ConcurrentHashMap<>();
+			//"plugins": {"plugin-name": {}}
+			JsonObject pluginData = remoteData.getAsJsonObject("plugins");
+			for (String key : pluginData.keySet()) {
+				pluginDataMap.put(key, pluginData.getAsJsonObject(key));
+			}
+			return pluginDataMap;
+		}
+
+		private JsonObject deserializePluginData() {
+			JsonObject pluginData = new JsonObject();
+			for (Map.Entry<String, JsonObject> entry : mPluginData.entrySet()) {
+				pluginData.add(entry.getKey(), entry.getValue());
+			}
+			return pluginData;
+		}
+
+		public void broadcast() {
+			JsonObject remotePlayerData = new JsonObject();
+			remotePlayerData.addProperty("playerUuid", mUuid.toString());
+			remotePlayerData.addProperty("playerName", mName);
+			remotePlayerData.addProperty("isHidden", mIsHidden);
+			remotePlayerData.addProperty("isOnline", mIsOnline);
+			remotePlayerData.addProperty("shard", mShard);
+
+			//Gather Plugin data
+			GatherRemotePlayerDataEvent event = new GatherRemotePlayerDataEvent();
+			Bukkit.getPluginManager().callEvent(event);
+			mPluginData.clear();
+			mPluginData.putAll(event.getPluginData());
+
+			remotePlayerData.add("plugins", deserializePluginData());
+
+			try {
+				NetworkRelayAPI.sendBroadcastMessage(RemotePlayerManager.REMOTE_PLAYER_UPDATE_CHANNEL, remotePlayerData);
+			} catch (Exception e) {
+				RemotePlayerManager manager = RemotePlayerManager.getInstance();
+				if (manager != null) {
+					manager.mPlugin.getLogger().warning("Failed to broadcast to channel " + REMOTE_PLAYER_UPDATE_CHANNEL + " reason: " + e.getMessage());
+				}
+			}
+		}
+	}
+
 	public static final String REMOTE_PLAYER_CHANNEL_BASE = "monumenta.redissync.remote_player";
-	public static final String REMOTE_PLAYER_UPDATE_CHANNEL = REMOTE_PLAYER_CHANNEL_BASE + ".update";
 	public static final String REMOTE_PLAYER_REFRESH_CHANNEL = REMOTE_PLAYER_CHANNEL_BASE + ".refresh";
+	public static final String REMOTE_PLAYER_UPDATE_CHANNEL = REMOTE_PLAYER_CHANNEL_BASE + ".update";
 
-	@MonotonicNonNull
-	private static RemotePlayerManager INSTANCE = null;
-
+	private static @MonotonicNonNull RemotePlayerManager INSTANCE = null;
 	private static final Map<String, Map<String, RemotePlayer>> mRemotePlayerShardMapped = new ConcurrentSkipListMap<>();
 	private static final Map<UUID, RemotePlayer> mRemotePlayersByUuid = new ConcurrentSkipListMap<>();
 	private static final Map<String, RemotePlayer> mRemotePlayersByName = new ConcurrentSkipListMap<>();
@@ -37,6 +112,7 @@ public class RemotePlayerManager implements Listener {
 	private final NetworkRelay mPlugin;
 
 	protected RemotePlayerManager(NetworkRelay plugin) {
+		INSTANCE = this;
 		mPlugin = plugin;
 		String lShard = getShardName();
 		try {
@@ -55,8 +131,6 @@ public class RemotePlayerManager implements Listener {
 		} catch (Exception e) {
 			mPlugin.getLogger().warning("Failed to broadcast to channel " + REMOTE_PLAYER_REFRESH_CHANNEL);
 		}
-
-		INSTANCE = this;
 	}
 
 	@Nullable
@@ -65,6 +139,25 @@ public class RemotePlayerManager implements Listener {
 			return null;
 		}
 		return INSTANCE;
+	}
+
+	@Nullable
+	protected String getShardName() {
+		@Nullable String shardName = null;
+		try {
+			shardName = NetworkRelayAPI.getShardName();
+		} catch (Exception e) {
+			mPlugin.getLogger().severe("Failed to retrieve shard name");
+		}
+		return shardName;
+	}
+
+	public static boolean isPlayerOnline(UUID playerUuid) {
+		return mRemotePlayersByUuid.containsKey(playerUuid);
+	}
+
+	public boolean isPlayerOnline(String playerName) {
+		return mRemotePlayersByName.containsKey(playerName);
 	}
 
 	protected Set<String> getAllOnlinePlayersName(boolean visibleOnly) {
@@ -112,6 +205,30 @@ public class RemotePlayerManager implements Listener {
 		return isPlayerVisible(playerUuid);
 	}
 
+	protected @Nullable String getPlayerName(@Nullable UUID playerUuid) {
+		if (playerUuid == null) {
+			return null;
+		}
+
+		@Nullable RemotePlayer remotePlayer = mRemotePlayersByUuid.get(playerUuid);
+		if (remotePlayer == null) {
+			return null;
+		}
+		return remotePlayer.mName;
+	}
+
+	protected @Nullable UUID getPlayerUuid(@Nullable String playerName) {
+		if (playerName == null) {
+			return null;
+		}
+
+		@Nullable RemotePlayer remotePlayer = mRemotePlayersByName.get(playerName);
+		if (remotePlayer == null) {
+			return null;
+		}
+		return remotePlayer.mUuid;
+	}
+
 	@Nullable
 	protected String getPlayerShard(@NotNull String username) {
 		@Nullable RemotePlayer remotePlayer = getRemotePlayer(username);
@@ -143,41 +260,6 @@ public class RemotePlayerManager implements Listener {
 
 		@Nullable RemotePlayer remotePlayer = mRemotePlayersByUuid.get(playerUuid);
 		return remotePlayer;
-	}
-
-	@Nullable
-	protected String getShardName() {
-		@Nullable String shardName = null;
-		try {
-			shardName = NetworkRelayAPI.getShardName();
-		} catch (Exception e) {
-			mPlugin.getLogger().severe("Failed to retrieve shard name");
-		}
-		return shardName;
-	}
-
-	protected @Nullable String getPlayerName(@Nullable UUID playerUuid) {
-		if (playerUuid == null) {
-			return null;
-		}
-
-		@Nullable RemotePlayer remotePlayer = mRemotePlayersByUuid.get(playerUuid);
-		if (remotePlayer == null) {
-			return null;
-		}
-		return remotePlayer.mName;
-	}
-
-	protected @Nullable UUID getPlayerUuid(@Nullable String playerName) {
-		if (playerName == null) {
-			return null;
-		}
-
-		@Nullable RemotePlayer remotePlayer = mRemotePlayersByName.get(playerName);
-		if (remotePlayer == null) {
-			return null;
-		}
-		return remotePlayer.mUuid;
 	}
 
 	protected void refreshLocalPlayers() {
@@ -315,84 +397,6 @@ public class RemotePlayerManager implements Listener {
 			}
 			default: {
 				break;
-			}
-		}
-	}
-
-	public static class RemotePlayer {
-		public final UUID mUuid;
-		public final String mName;
-		public final String mShard;
-		public final boolean mIsHidden;
-		public final boolean mIsOnline;
-		private final ConcurrentMap<String, JsonObject> mPluginData;
-
-		public RemotePlayer(Player player, boolean isOnline) {
-			mUuid = player.getUniqueId();
-			mName = player.getName();
-			RemotePlayerManager manager = RemotePlayerManager.getInstance();
-			mShard = (manager != null && manager.getShardName() != null) ? manager.getShardName() : NetworkRelayAPI.getShardName();
-			mIsHidden = (manager != null && !manager.isLocalPlayerVisible(player));
-			mIsOnline = isOnline;
-			mPluginData = new ConcurrentHashMap<>();
-		}
-
-		public RemotePlayer(JsonObject remoteData) {
-			mUuid = UUID.fromString(remoteData.get("playerUuid").getAsString());
-			mName = remoteData.get("playerName").getAsString();
-			mIsHidden = remoteData.get("isHidden").getAsBoolean();
-			mIsOnline = remoteData.get("isOnline").getAsBoolean();
-			mShard = remoteData.get("shard").getAsString();
-			mPluginData = serializePluginData(remoteData);
-		}
-
-		@Nullable
-		public JsonObject getPluginData(String pluginId) {
-			return mPluginData.get(pluginId);
-		}
-
-		private ConcurrentMap<String, JsonObject> serializePluginData(JsonObject remoteData) {
-			ConcurrentMap<String, JsonObject> pluginDataMap = new ConcurrentHashMap<>();
-			//"plugins": {"plugin-name": {}}
-			JsonObject pluginData = remoteData.getAsJsonObject("plugins");
-			for (String key : pluginData.keySet()) {
-				pluginDataMap.put(key, pluginData.getAsJsonObject(key));
-			}
-			return pluginDataMap;
-		}
-
-		private JsonObject deserializePluginData() {
-			JsonObject pluginData = new JsonObject();
-			for (Map.Entry<String, JsonObject> entry : mPluginData.entrySet()) {
-				pluginData.add(entry.getKey(), entry.getValue());
-			}
-			return pluginData;
-		}
-
-		public void broadcast() {
-			JsonObject remotePlayerData = new JsonObject();
-			remotePlayerData.addProperty("playerUuid", mUuid.toString());
-			remotePlayerData.addProperty("playerName", mName);
-			remotePlayerData.addProperty("isHidden", mIsHidden);
-			remotePlayerData.addProperty("isOnline", mIsOnline);
-			remotePlayerData.addProperty("shard", mShard);
-
-			//Gather Plugin data
-			GatherRemotePlayerDataEvent event = new GatherRemotePlayerDataEvent();
-			Bukkit.getPluginManager().callEvent(event);
-			mPluginData.clear();
-			mPluginData.putAll(event.getPluginData());
-
-			remotePlayerData.add("plugins", deserializePluginData());
-
-			try {
-				NetworkRelayAPI.sendBroadcastMessage(RemotePlayerManager.REMOTE_PLAYER_UPDATE_CHANNEL,
-					remotePlayerData);
-			} catch (Exception e) {
-				RemotePlayerManager manager = RemotePlayerManager.getInstance();
-				if (manager != null) {
-					manager.mPlugin.getLogger().warning("Failed to broadcast to channel " + REMOTE_PLAYER_UPDATE_CHANNEL + " reason: " + e.getMessage());
-				}
 			}
 		}
 	}
