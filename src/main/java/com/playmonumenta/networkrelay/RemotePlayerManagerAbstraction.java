b45f5a73599eca7f7dad29c57020e21d5c543d97
Ascynx <miaoum2128@gmail.com>
2024-02-07 22:52:05 +0100

    maybe this will do

--- src/main/java/com/playmonumenta/networkrelay/RemotePlayerManagerAbstraction.java
+++ src/main/java/com/playmonumenta/networkrelay/RemotePlayerManagerAbstraction.java
@@ -24,18 +24,22 @@ public abstract class RemotePlayerManagerAbstraction {
 	protected abstract Set<String> getAllOnlinePlayersName(boolean visibleOnly);
 
 	protected abstract boolean isPlayerOnline(String playerName);
+
 	protected abstract boolean isPlayerOnline(UUID playerUuid);
 
 	@Nullable
 	protected abstract  String getPlayerShard(String playerName);
+
 	@Nullable
 	protected abstract String getPlayerShard(UUID playerUuid);
 
 	@Nullable
 	protected abstract RemotePlayerAbstraction getRemotePlayer(String playerName);
+
 	@Nullable
 	protected abstract RemotePlayerAbstraction getRemotePlayer(UUID playerUuid);
 
 	protected abstract boolean isPlayerVisible(String playerName);
+
 	protected abstract boolean isPlayerVisible(UUID playerUuid);
 }
