a01772eaa409623cb42f561fdcba895e68275637
Timothy Southwick <NickNackGus@gmail.com>
2024-05-20 16:19:06 -0400

    Unused imports

--- src/main/java/com/playmonumenta/networkrelay/WhereIsCommandBungee.java
+++ src/main/java/com/playmonumenta/networkrelay/WhereIsCommandBungee.java
@@ -1,7 +1,6 @@
 package com.playmonumenta.networkrelay;
 
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
