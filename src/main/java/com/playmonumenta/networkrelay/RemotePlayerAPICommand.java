82c2536dc9c1a137226267b1f0d08d25ac75dca7
Timothy Southwick <NickNackGus@gmail.com>
2024-09-05 17:51:34 -0400

    Added command to refresh player in RPM (#35)

--- src/main/java/com/playmonumenta/networkrelay/RemotePlayerAPICommand.java
+++ src/main/java/com/playmonumenta/networkrelay/RemotePlayerAPICommand.java
@@ -3,23 +3,28 @@ package com.playmonumenta.networkrelay;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.ArgumentSuggestions;
+import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.StringArgument;
+import java.util.Collection;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public abstract class RemotePlayerAPICommand {
 	public static final ArgumentSuggestions<CommandSender> VISIBLE_PLAYER_ONLINE_SUGGESTIONS = ArgumentSuggestions.strings((unused) -> NetworkRelayAPI.getVisiblePlayerNames().toArray(String[]::new));
 
+	@SuppressWarnings("unchecked")
 	public static void register() {
 		// we use all here for moderators
-		Argument<String> playerArg = new StringArgument("player").replaceSuggestions(VISIBLE_PLAYER_ONLINE_SUGGESTIONS);
+		Argument<String> remotePlayerArg = new StringArgument("player").replaceSuggestions(VISIBLE_PLAYER_ONLINE_SUGGESTIONS);
+		Argument<?> localPlayerArg = new EntitySelectorArgument.ManyPlayers("player");
 
 		// moderator command
 		new CommandAPICommand("remoteplayerapi")
 			.withPermission("monumenta.networkrelay.remoteplayerapi")
 			.withSubcommand(new CommandAPICommand("get")
-				.withArguments(playerArg)
+				.withArguments(remotePlayerArg)
 				.executes((sender, args) -> {
-					String playerName = args.getByArgument(playerArg);
+					String playerName = args.getByArgument(remotePlayerArg);
 					RemotePlayerData data = RemotePlayerAPI.getRemotePlayer(playerName);
 					if (data == null) {
 						sender.sendMessage("No data found: " + playerName);
@@ -28,7 +33,16 @@ public abstract class RemotePlayerAPICommand {
 					sender.sendMessage(data.toString());
 				})
 			)
+			.withSubcommand(new CommandAPICommand("refreshlocalplayer")
+				.withArguments(localPlayerArg)
+				.executes((sender, args) -> {
+					Collection<Player> targetPlayers = (Collection<Player>) args.getByArgument(localPlayerArg);
+					for (Player targetPlayer : targetPlayers) {
+						RemotePlayerAPI.refreshPlayer(targetPlayer.getUniqueId());
+					}
+					return targetPlayers.size();
+				})
+			)
 			.register();
-
 	}
 }
