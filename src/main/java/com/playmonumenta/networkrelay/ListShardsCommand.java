a68fbd6e00bbad673fa4ac06e6fd994862507dd5
Timothy Southwick <NickNackGus@gmail.com>
2025-06-16 12:33:59 -0400

    Include proxy-shard pings in /monumenta networkRelay listShards

--- src/main/java/com/playmonumenta/networkrelay/ListShardsCommand.java
+++ src/main/java/com/playmonumenta/networkrelay/ListShardsCommand.java
@@ -2,21 +2,48 @@ package com.playmonumenta.networkrelay;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.CommandPermission;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeSet;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
 
 public class ListShardsCommand {
 	protected static void register() {
 		CommandAPICommand innerCommand = new CommandAPICommand("listShards")
 			.withPermission(CommandPermission.fromString("monumenta.networkrelay.listshards"))
 			.executes((sender, args) -> {
-				try {
-					TreeSet<String> shardNames = new TreeSet<>(NetworkRelayAPI.getOnlineShardNames());
-					sender.sendMessage(Component.text("Online shards: " + shardNames, NamedTextColor.GOLD));
-				} catch (Exception e) {
-					sender.sendMessage(Component.text("An error occurred, cannot check online shards.", NamedTextColor.RED));
+				String senderProxy;
+				if (sender instanceof Player player) {
+					senderProxy = RemotePlayerAPI.getPlayerProxy(player.getUniqueId());
+				} else {
+					senderProxy = null;
 				}
+
+				TreeSet<String> shardNames = new TreeSet<>(NetworkRelayAPI.getOnlineShardNames());
+
+				List<Component> shardComponents = new ArrayList<>();
+				for (String shardName : shardNames) {
+					if (senderProxy == null) {
+						shardComponents.add(Component.text(shardName));
+					} else {
+						Long pingMs = NetworkRelayAPI.getProxyToShardPingMs(senderProxy, shardName);
+						Component pingComponent;
+						if (pingMs == null) {
+							pingComponent = Component.text("Timed Out (" + senderProxy + ")");
+						} else {
+							pingComponent = Component.text(pingMs + " ms (" + senderProxy + ")");
+						}
+						shardComponents.add(Component.text(shardName)
+							.hoverEvent(pingComponent));
+					}
+				}
+
+				sender.sendMessage(Component.text("Online shards: ", NamedTextColor.GOLD)
+					.append(Component.join(JoinConfiguration.commas(true), shardComponents))
+				);
 			});
 
 		// Register first under the monumenta -> networkRelay namespace
